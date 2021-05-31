eb5efb19f10a56a891969a6eb2f4150729eb17b6
Raghu Pratap Singh <raghupratapsingh91199@gmail.com>
2021-05-31 20:59:02 +0530

    static root error resolved

--- Eshop/urls.py
+++ Eshop/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path , include
-from django.conf.urls.static import static
-from . import settings
+
 
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 
 urlpatterns = [
