42e8044cc101c5dd994d98f2c62a4e83d9765cc5
Raghu Pratap Singh <raghupratapsingh91199@gmail.com>
2021-05-31 20:41:15 +0530

    requirement file added

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '4sr!x=o*3)!#zst77m!9$g=9$%0*e4@tj9(8a=4tinqh77u!0+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = false
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
