6b61e8e8d8a29004cfe7bdabbf1166b0e89d63c5
Raghu Pratap Singh <raghupratapsingh91199@gmail.com>
2021-05-31 22:02:10 +0530

    allowed hosts updated

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = '4sr!x=o*3)!#zst77m!9$g=9$%0*e4@tj9(8a=4tinqh77u!0+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'skmobiles.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'skmobiles.herokuapp.com']
 
 
 # Application definition
